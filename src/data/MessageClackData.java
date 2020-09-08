93f0afe56a31b56b7fc27741bc3f016ea1c15bf0
Jeff Rescignano <jeff@jeffresc.dev>
2020-09-07 20:28:31 -0400

    Created class structure for MessageClackData

--- src/data/MessageClackData.java
+++ src/data/MessageClackData.java
@@ -1,5 +1,17 @@
 package data;
 
-public class MessageClackData {
-
+public class MessageClackData extends ClackData {
+	
+	// ClackData variables
+	String message;
+	
+	public MessageClackData(String userName, String message, Integer type) {
+		super();
+		this.userName = userName;
+		this.type = type;
+	}
+	
+	public MessageClackData() {
+		//MessageClackData();
+	}
 }
