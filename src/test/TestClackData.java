ad725658a38e0e62cdd5bb30785dec757a1b43c8
dwarv <dwarv@128.153.184.230>
2020-09-08 21:41:00 -0400

    implemented TestClack Data might have to add more.

--- src/test/TestClackData.java
+++ src/test/TestClackData.java
@@ -1,5 +1,22 @@
 package test;
 
-public class TestClackData {
+import data.ClackData;
+import data.FileClackData;
+import data.MessageClackData;
 
+public class TestClackData {
+	
+	public static void main( String args []) {
+		ClackData fileClackData = new FileClackData();
+		fileClackData.getType();
+		fileClackData.getUserName();
+		fileClackData.getDate();
+		fileClackData.getData();
+		
+		ClackData messageClackData = new MessageClackData();
+		messageClackData.getType();
+		messageClackData.getUserName();
+		messageClackData.getDate();
+		messageClackData.getData();
+	}
 }
