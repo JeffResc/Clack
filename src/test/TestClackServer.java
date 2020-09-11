10c171ba61c1832489883fb04103109525f83802
Jeff Rescignano <jeff@jeffresc.dev>
2020-09-11 01:01:39 -0400

    Implemented TestClackServer

--- src/test/TestClackServer.java
+++ src/test/TestClackServer.java
@@ -1,5 +1,21 @@
 package test;
 
-public class TestClackServer {
+import main.ClackServer;
 
+public class TestClackServer {
+	public static void main(String[] args) {
+		System.out.println("ClackServer1:");
+		ClackServer ClackServer1 = new ClackServer();
+		System.out.println(ClackServer1.getPort());
+		System.out.println(ClackServer1.toString());
+		System.out.println("----------");
+		
+		System.out.println("ClackServer2:");
+		ClackServer ClackServer2 = new ClackServer(3000);
+		System.out.println(ClackServer2.getPort());
+		System.out.println(ClackServer2.toString());
+		System.out.println(ClackServer2.equals(ClackServer1));
+		System.out.println("----------");
+	}
+	
 }
