b900fe2aceb01df0c7912f8a945bf1e0b500f739
Jeff Rescignano <jeff@jeffresc.dev>
2020-09-11 01:49:40 -0400

    Properly implement TestClackData

--- src/test/TestClackData.java
+++ src/test/TestClackData.java
@@ -7,16 +7,42 @@ import data.MessageClackData;
 public class TestClackData {
 
 	public static void main(String args[]) {
-		ClackData fileClackData = new FileClackData();
-		fileClackData.getType();
-		fileClackData.getUserName();
-		fileClackData.getDate();
-		fileClackData.getData();
+		System.out.println("ClackData1:");
+		ClackData ClackData1 = new FileClackData("Jeff", "Hello, world!", 0);
+		System.out.println(ClackData1.getType());
+		System.out.println(ClackData1.getUserName());
+		System.out.println(ClackData1.getDate());
+		System.out.println(ClackData1.getData());
+		System.out.println(ClackData1.toString());
+		System.out.println("");
 
-		ClackData messageClackData = new MessageClackData();
-		messageClackData.getType();
-		messageClackData.getUserName();
-		messageClackData.getDate();
-		messageClackData.getData();
+		System.out.println("ClackData2:");
+		ClackData ClackData2 = new FileClackData();
+		System.out.println(ClackData2.getType());
+		System.out.println(ClackData2.getUserName());
+		System.out.println(ClackData2.getDate());
+		System.out.println(ClackData2.getData());
+		System.out.println(ClackData2.toString());
+		System.out.println(ClackData2.equals(ClackData1));
+		System.out.println("");
+		
+		System.out.println("ClackData3:");
+		ClackData ClackData3 = new MessageClackData("Jeff", "Hello, world!", 0);
+		System.out.println(ClackData3.getType());
+		System.out.println(ClackData3.getUserName());
+		System.out.println(ClackData3.getDate());
+		System.out.println(ClackData3.getData());
+		System.out.println(ClackData3.toString());
+		System.out.println("");
+		
+		System.out.println("ClackData4:");
+		ClackData ClackData4 = new MessageClackData();
+		System.out.println(ClackData4.getType());
+		System.out.println(ClackData4.getUserName());
+		System.out.println(ClackData4.getDate());
+		System.out.println(ClackData4.getData());
+		System.out.println(ClackData4.toString());
+		System.out.println(ClackData4.equals(ClackData3));
+		System.out.println("");
 	}
 }
