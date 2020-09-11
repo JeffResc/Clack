89fd75d7080869438c4d96c022b86ac114fd57f9
Jeff Rescignano <jeff@jeffresc.dev>
2020-09-11 01:16:34 -0400

    Format output of TestClackServer

--- src/test/TestClackServer.java
+++ src/test/TestClackServer.java
@@ -9,14 +9,13 @@ public class TestClackServer {
 		ClackServer ClackServer1 = new ClackServer();
 		System.out.println(ClackServer1.getPort());
 		System.out.println(ClackServer1.toString());
-		System.out.println("----------");
+		System.out.println("");
 
 		System.out.println("ClackServer2:");
 		ClackServer ClackServer2 = new ClackServer(3000);
 		System.out.println(ClackServer2.getPort());
 		System.out.println(ClackServer2.toString());
 		System.out.println(ClackServer2.equals(ClackServer1));
-		System.out.println("----------");
 	}
 	
 }
