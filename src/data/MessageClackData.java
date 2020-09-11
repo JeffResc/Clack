79d2385d746a33fc45538e239639ebed2f6b288c
Jeff Rescignano <jeff@jeffresc.dev>
2020-09-11 01:10:19 -0400

    Implement hashCode() and equals() in MessageClackData

--- src/data/MessageClackData.java
+++ src/data/MessageClackData.java
@@ -27,17 +27,32 @@ public class MessageClackData extends ClackData {
 	public String getData() {
 		return message;
 	}
-	
+
 	@Override
-	public boolean equals (Object other) {
-		if ( other == null) return false; // makes sure the other object isnt null
-		if ( !(other instanceof MessageClackData)) return false; // makes sure that the other object points to an object of the same type
-		
-		MessageClackData otherMessageClackData = (MessageClackData)other; // creates a circle object and sets it = to the other object
-		
-		return this.message == otherMessageClackData.getData();
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((message == null) ? 0 : message.hashCode());
+		return result;
 	}
-	
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		MessageClackData other = (MessageClackData) obj;
+		if (message == null) {
+			if (other.message != null)
+				return false;
+		} else if (!message.equals(other.message))
+			return false;
+		return true;
+	}
+
 	@Override
 	public String toString() { // when you print out an object it looks for the toString method which is overridden from the super class Object 
 		return "message: " + message; 
