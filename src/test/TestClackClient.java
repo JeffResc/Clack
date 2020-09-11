e98a15d5800d11e1026706ed5346cb6e8fa0969d
Jeff Rescignano <jeff@jeffresc.dev>
2020-09-11 01:16:59 -0400

    Format output of TestClackClient

--- src/test/TestClackClient.java
+++ src/test/TestClackClient.java
@@ -11,7 +11,7 @@ public class TestClackClient {
 		System.out.println(ClackClient1.getHostName());
 		System.out.println(ClackClient1.getPort());
 		System.out.println(ClackClient1.toString());
-		System.out.println("----------");
+		System.out.println("");
 
 		System.out.println("ClackClient2:");
 		ClackClient ClackClient2 = new ClackClient("Anon");
@@ -20,7 +20,7 @@ public class TestClackClient {
 		System.out.println(ClackClient2.getPort());
 		System.out.println(ClackClient2.toString());
 		System.out.println(ClackClient2.equals(ClackClient1));
-		System.out.println("----------");
+		System.out.println("");
 
 		System.out.println("ClackClient3:");
 		ClackClient ClackClient3 = new ClackClient("Jeff", "server.jeffresc.dev");
@@ -29,7 +29,7 @@ public class TestClackClient {
 		System.out.println(ClackClient3.getPort());
 		System.out.println(ClackClient3.toString());
 		System.out.println(ClackClient3.equals(ClackClient1));
-		System.out.println("----------");
+		System.out.println("");
 
 		System.out.println("ClackClient4:");
 		ClackClient ClackClient4 = new ClackClient("Jeff", "server.jeffresc.dev", 3000);
@@ -37,7 +37,6 @@ public class TestClackClient {
 		System.out.println(ClackClient4.getHostName());
 		System.out.println(ClackClient4.getPort());
 		System.out.println(ClackClient4.toString());
-		System.out.println("----------");
 	}
 	
 }
