6662846092b6436e3fc0b94daa70e7a8c7895014
Jeff Rescignano <jeff@jeffresc.dev>
2020-09-11 19:21:43 -0400

    Edit JavaDoc comments in MessageClackData

--- src/data/MessageClackData.java
+++ src/data/MessageClackData.java
@@ -11,6 +11,10 @@ public class MessageClackData extends ClackData {
 
 	/**
 	 * Constructor that accepts a userName, message and type.
+	 * 
+	 * @param userName userName
+	 * @param message message
+	 * @param type type
 	 */
 	public MessageClackData(String userName, String message, Integer type) {
 		super(userName, type);
@@ -27,17 +31,23 @@ public class MessageClackData extends ClackData {
 	/**
 	 * Returns the method.
 	 * 
-	 * @return
+	 * @return message
 	 */
 	public String getData() {
 		return message;
 	}
 
+	/**
+	 * Implements hashCode() functionality for this class and it's superclass' variables.
+	 */
 	@Override
 	public int hashCode() {
 		return Objects.hash(message, getUserName(), getType(), getDate());
 	}
 
+	/**
+	 * Implements equals() functionality for this class and it's superclass' variables.
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -50,6 +60,9 @@ public class MessageClackData extends ClackData {
 		return Objects.equals(message, other.message) && Objects.equals(getUserName(), other.getUserName()) && Objects.equals(getType(), other.getType()) && Objects.equals(getDate(), other.getDate());
 	}
 
+	/**
+	 * Implements toString() functionality for this class' variables.
+	 */
 	@Override
 	public String toString() {
 		return "MessageClackData [message=" + message + "]";
